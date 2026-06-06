db0413929b118b3973b59a3cb0d29feab742bc42
frahim arif <frahim9900@gmail.com>
2026-06-06 17:07:50 +0530

    chang

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../../controllers/admin/auth.controller");
+const authController = require("../controllers/admin/auth.controller");
 
 // Register - use once via curl or protect via other means
 router.post("/register", authController.registerAdmin);
