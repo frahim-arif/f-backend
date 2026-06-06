ab8e8596b33b29e5ba66032f6e48b1bc3668d0ed
frahim arif <frahim9900@gmail.com>
2026-06-06 17:31:27 +0530

    chang

--- app/controllers/admin/question.controller.js
+++ app/controllers/admin/question.controller.js
@@ -37,7 +37,7 @@ const corrections = {
   althiat: "tahiyyat",bch: "bachche", wal: "wale", dn: "din", rka: "rakha", gya: "gaya", tyn: "teen", many: "maani",
   mi: "mein",pir: "pair", kte: "katte", hoe: "huye", msnai: "masnoi", lgate: "lagate", ozo: "wuzu",
   chtai: "chatai", swal: "sawal", hsh: "hissa", w: "aur", s: "se", k: "ke",
-  aor: "aur",ghnte: "ghante", oaste: "waste", bahr: "bahar", nkl: "nikal",
+  aor: "aur",ghnte: "ghante", oaste: "waste", bahr: "bahar", nkl: "nikal",mindk: "mendak", drhm: "dirham", ghir: "ghair", mslm: "muslim", pish: "paisa", kndidits: "candidates",
   qalin: "qaleen", zrort: "zarurat", zad: "ziyada", he: "hai", kia: "kiya", shkhs: "shakhs", anjmn: "anjuman", ia: "ya", akidmi: "academy", qam: "qaim", kya: "kya", as: "us",
   bioi: "biwi", bad: "baad", jmaat: "jamaat", phle: "pehle", wzw: "wuzu", awr: "aur", abart: "ibarat",
   lrai: "larai", chl: "chal", rhi: "rahi", ayk: "aik", shkhs: "shakhs", sna: "suna", he: "hai", kh: "ke", awrty: "aurat", jb: "jab", sal: "saal", gzrne: "guzarne",
