aa81d56f527544eca0aef478e9ac1b5ed18fb00a
frahim arif <frahim9900@gmail.com>
2026-06-30 05:52:23 +0530

    cvgfd

--- app/controllers/admin/question.controller.js
+++ app/controllers/admin/question.controller.js
@@ -21,7 +21,7 @@ const corrections = {
   ih: "yeh", dhli: "delhi", ja: "jaunga", mire: "mere", baozo: "bawuzu", mchhr: "machhar", kata: "kaata", khon: "khoon", chos: "choos",
   atkaf: "itikaf", snt: "sunnat", frq: "farq", oazh: "wazeh", frmaie: "farmaiye", qsmi: "qismein", frz: "farz", oajb: "wajib", msthb: "mustahab", an: "aur",
   chlne: "chalne",lrka: "ladka", fon: "phone", oidio: "audio", kl: "call", zriah: "zariye", aijab: "ijab",oaldin: "walidain", sb: "sab", sath: "saath", rhte: "rahte", aolad: "aulad", km: "kama",
-  phrne: "phirne",nkah: "nikah", moqa: "mauqa", lrki: "ladki", ajazt: "ijazat", line: "lene", okil: "wakeel", goahan: "gawahan",
+  phrne: "phirne",nkah: "nikah", moqa: "mauqa", lrki: "ladki", ajazt: "ijazat", line: "lene", okil: "wakeel", goahan: "gawahan",zamen: "zameen", gi: "gayi", js: "jis",
   sholt: "sahulat",hafz: "hafiz", qran: "quran", rmzan: "ramzan", ashrh: "ashrah", bith: "baith", gia: "gaya",dhato: "dhatuon", mjsmh: "mujassamah", khrid: "khareed", frokht: "farokht", nhi: "nahi", asi: "aisi",
   lie: "liye",zid: "zaid", amr: "umar", mkan: "makaan", khrida: "khareeda", khridar: "khareedar", kchh: "kuch",shdidh: "shadeed", ojh: "wajah", oqf: "waqf", zmin: "zameen", tbadlh: "tabadlah",
   shrai: "shar-i",jnazh: "janazah", tkbirat: "takbeerat", chhor: "chhod", slam: "salam", afzl: "afzal", khareda: "khareeda", jaunga: "ja raha", rha: "raha", kmi: "kami", bishi: "beshi",
